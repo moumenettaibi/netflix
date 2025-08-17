8815df4c41a7eae3ab30d18a855eaf74ccf7d7e5
Moumen <moumenettaibi746@gmail.com>
2025-08-17 02:16:25 +0100

    fix: correct template folder path

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 
 # Root route - redirect to browse
 @app.route("/")
